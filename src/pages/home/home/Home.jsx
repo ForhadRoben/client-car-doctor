dac77f45f2b11714225d6af6c6d4775026c37d4c
ForhadRoben <i.rforhad@gmail.com>
2023-05-16 18:32:44 +0600

    service card commmited

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,11 +1,14 @@
 import About from "../about/About";
 import Banner from "../banner/Banner";
+import Services from "../services/Services"
+
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <About></About>
+            <Services></Services>
         </div>
     );
 };
--- src/pages/shared/footer/Footer.jsx
+++ src/pages/shared/footer/Footer.jsx
@@ -6,7 +6,7 @@ import logo from '../../../assets/logo.svg'
 const Footer = () => {
     return (
         <div>
-            <footer className="footer p-10 bg-base-200 text-base-content">
+            <footer className="footer p-10 bg-base-200 mt-5 text-base-content">
                 <div>
                     <img src={logo} alt="title_picture" />
                     <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
